71c135d744bf598cf2e1a10203d11ea582170c14
zp <>
2020-12-21 21:54:37 +0800

    19

--- week19/publish-server/server.js
+++ week19/publish-server/server.js
@@ -1,19 +1,80 @@
 let http = require('http')
+let https = require('https')
+let unzipper = require('unzipper')
+let querystring = require('querystring')
+// let fs = require('fs')
 
-let fs = require('fs')
+// 2. auth路由：接收code，用code+client_id+client_secret换token
+function auth (request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1])
+    getToken(query.code, function (info) {
+        console.log(info)
+        // response.write(JSON.stringify(info))
+        response.write(`<a href='http://localhost:8083/?token=${info.access_token}'>publish</a>`)
+        response.end()
+    })
+}
+
+function getToken (code, callback) {
+    let request = https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}&client_id=Iv1.509dbf8bb5da1040&client_secret=9674d0c393fad46f34a9522b3b6cb68af770f923`,
+        port: 443,
+        method: 'POST'
+    }, function (response) {
+        let body = ''
+        response.on('data', chunk => {
+            body += chunk.toString()
+        })
+        response.on('end', () => {
+            callback(querystring.parse(body))
+        })
+    })
+    request.end()
+}
+
+// 4. publish路由：用token获取用户信息，检查权限，接受发布
+function publish (request, response) {
+    let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1])
+    getUser(query.token, info => {
+        if (info.login === 'luckyAlexandra') { // 这里正常情况下是要接一个权限系统的
+            request.pipe(unzipper.Extract({path: '../server/public/'}))
+            request.on('end', function () {
+                response.end('success')
+            })
+        }
+    })
+}
+
+function getUser (token, callback) {
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: `/user`,
+        port: 443,
+        method: 'GET',
+        headers: {
+            'Authorization': `token ${token}`,
+            'User-Agent': 'alex-toy-publish'
+        }
+    }, function (response) {
+        let body = ''
+        response.on('data', chunk => {
+            body += chunk.toString()
+        })
+        response.on('end', () => {
+            callback(JSON.parse(body))
+        })
+    })
+    request.end()
+}
+
+
 
 http.createServer(function (request, response) {
-    console.log(request.headers)
-
-    let outFile = fs.createWriteStream('../server/public/index.html')
-
-    request.on('data', chunk => {
-        // console.log(chunk.toString())
-        outFile.write(chunk)
-    })
-    request.on('end', () => {
-        outFile.end()
-        response.end('Success')
-    })
-    // res.end('hello world')
+    if (request.url.match(/^\/auth\?/)) {
+        return auth(request, response)
+    }
+    if (request.url.match(/^\/publish\?/)) {
+        return publish(request, response)
+    }
 }).listen(8082)
--- week19/publish-tool/publish.js
+++ week19/publish-tool/publish.js
@@ -1,29 +1,45 @@
 let http = require('http')
-
-// 引入文件系统的包
 let fs = require('fs')
+let archiver = require('archiver')
+let child_process = require('child_process')
+let querystring = require('querystring')
 
-let request = http.request({
-    hostname: '127.0.0.1',
-    port: 8082,
-    // port: 8882,
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/octet-stream'
-    }
-}, response => {
-    console.log(response)
-})
+// 1. 打开 https://github.com/login/oauth/authorize
+child_process.exec(`open https://github.com/login/oauth/authorize?client_id=Iv1.509dbf8bb5da1040`)
+
+// 3. 创建server，接受token，后点击发布。
+
+http.createServer(function (request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1])
+    publish(query.token)
+}).listen(8083)
+
+function publish (token) {
+    let request = http.request({
+        hostname: '127.0.0.1',
+        port: 8082,
+        // port: 8882,
+        method: 'POST',
+        path: '/publish?token=' + token,
+        headers: {
+            'Content-Type': 'application/octet-stream'
+            // 'Content-Length': stats.size
+        }
+    }, response => {
+        console.log(response)
+    })
+    
+    const archive = archiver('zip', {
+        zlib: { level: 9 } // Sets the compression level.
+    });
+    
+    archive.directory('./sample', false)
+    
+    archive.finalize()
+    
+    archive.pipe(request)
+}
 
 
-let file = fs.createReadStream('./sample.html')
 
-file.on('data', chunk => {
-    console.log(chunk.toString())
-    request.write(chunk)
-})
 
-file.on('end', chunk => {
-    console.log('read finished')
-    request.end(chunk)
-})
